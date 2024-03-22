d144e26ffe203b76fa7376ebd8d20b920715ce8a
SR ITJGEC <snigdho386@gmail.com>
2024-03-22 22:02:37 +0530

    Setup of frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const chats = require('./data/data')
+const dotenv = require('dotenv')
+
+dotenv.config() 
 
 const app = express()
- 
+
+
 app.get("/", (req,res)=>{
     res.send("API is running !")
 })
@@ -10,3 +14,15 @@ app.get("/", (req,res)=>{
 app.get('/api/chat',(req,res)=>{
     res.send(chats)
 })
+app.get('api/chat/:id', (req,res)=>{
+    const chatId = req.params.id 
+    const chat = chats.find(c=>c.id==chatId)
+})
+
+const PORT = process.env.PORT||5000
+
+app.listen(PORT,()=>{
+    console.log(`Server started in Port ${PORT} `)
+})
+
+console.log(chats)
